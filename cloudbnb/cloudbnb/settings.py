9517561cbaa77bf784195f3478ac914d6cb1f095
Andy-Nkumane <nkumaneandy@gmail.com>
2020-11-12 13:52:23 +0200

    added postgres settings

--- cloudbnb/cloudbnb/settings.py
+++ cloudbnb/cloudbnb/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'cloudbnb.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'db',
+        'USER': 'vector_user',
+        'PASSWORD': 'vector_password',
+        'HOST': '127.0.0.1',
+        'PORT': 6543, 
     }
 }
 
