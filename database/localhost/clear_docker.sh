ff0d7018ecbe0e4bbed79fb47a24bf070d09a323
Andy-Nkumane <nkumaneandy@gmail.com>
2020-11-12 13:55:26 +0200

    added docker files

---
+++ database/localhost/clear_docker.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker container stop $(docker container ls -aq)
+docker container rm $(docker container ls -aq)
+docker image rm $(docker image ls -aq)
---
+++ database/localhost/docker-entrypoint-initdb.d/create-test-db.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE test_db;
+grant all privileges on database test_db to vector_user;
+-- ALTER USER user WITH SUPERUSER;
