2d7c86da3d25402797bc5afe50addc6534646d78
Andy-Nkumane <nkumaneandy@gmail.com>
2020-11-14 14:46:45 +0200

    registered property model and property image model inline

--- cloudbnb/app_cloudbnb/admin.py
+++ cloudbnb/app_cloudbnb/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+class PropertyImageAdmin(admin.StackedInline):
+    model = models.PropertyImage
+
+@admin.register(models.Property)
+class PropertyAdmin(admin.ModelAdmin):
+    inlines = [PropertyImageAdmin]
+
+    class Meta:
+        model = models.Property
