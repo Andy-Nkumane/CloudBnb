aec036d686e98c9be5eb47eb744741699342ae72
Andy-Nkumane <nkumaneandy@gmail.com>
2020-11-14 14:42:27 +0200

    created property and property images models

--- cloudbnb/app_cloudbnb/models.py
+++ cloudbnb/app_cloudbnb/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from taggit.managers import TaggableManager
 
+class Property(models.Model):
+    description = models.TextField()
+    # pictures = models.ImageField(null=True)
+    city = models.CharField(max_length = 30)
+    surburb = models.CharField(max_length=30)
+    street_address = models.CharField(max_length=30)
+    max_guest = models.IntegerField()
+    guest_price_night = models.DecimalField(max_digits=10 ,decimal_places=2)
+    extra_guest_price_night = models.DecimalField(max_digits=10 ,decimal_places=2)
+    tags = TaggableManager()
 
+class PropertyImage(models.Model):
+    property_image = models.ForeignKey(Property, default=None, on_delete=models.CASCADE)
+    pictures = models.ImageField(upload_to='pictures/')
